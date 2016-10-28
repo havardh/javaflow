d610c749160bbad7aa35d7ab9f2fe7dceeaa2854
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2016-10-28 14:32:49 +0200

    refactor: extract type creation form class definition builder

--- src/main/java/no/havard/javaflow/model/builders/ClassDefinitionBuilder.java
+++ src/main/java/no/havard/javaflow/model/builders/ClassDefinitionBuilder.java
@@ -1,41 +1,17 @@
 package no.havard.javaflow.model.builders;
 
-import static no.havard.javaflow.model.Type.list;
-import static no.havard.javaflow.model.Type.map;
-import static no.havard.javaflow.model.Type.object;
-import static no.havard.javaflow.util.Maps.entriesToMap;
-import static no.havard.javaflow.util.Maps.entry;
+import static no.havard.javaflow.convertion.TypeFactory.factory;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
 
 import no.havard.javaflow.model.CanonicalName;
 import no.havard.javaflow.model.ClassDefinition;
 import no.havard.javaflow.model.FieldDefinition;
-import no.havard.javaflow.model.Type;
-
-import com.github.javaparser.ast.type.PrimitiveType;
 
 public class ClassDefinitionBuilder implements Builder<ClassDefinition> {
-
-  private static Map<String, String> BUILTIN = Collections.unmodifiableMap(Stream.of(
-      entry("Boolean", "java.lang"),
-      entry("Byte", "java.lang"),
-      entry("Character", "java.lang"),
-      entry("Double", "java.lang"),
-      entry("Float", "java.lang"),
-      entry("Integer", "java.lang"),
-      entry("Long", "java.lang"),
-      entry("Short", "java.lang"),
-      entry("String", "java.lang"),
-
-      entry("List", "java.util"),
-      entry("Map", "java.util")
-  ).collect(entriesToMap()));
 
   private String packageName;
   private String name;
@@ -71,9 +47,10 @@ public class ClassDefinitionBuilder implements Builder<ClassDefinition> {
   }
 
   public ClassDefinitionBuilder withField(com.github.javaparser.ast.type.Type type, String name) {
-    Type t = of(type);
-
-    this.fields.add(new FieldDefinition(name, t));
+    this.fields.add(new FieldDefinition(
+        name,
+        factory(packageName, imports).of(type)
+    ));
     return this;
   }
 
@@ -93,63 +70,7 @@ public class ClassDefinitionBuilder implements Builder<ClassDefinition> {
   }
 
   private String resolvePackageName(String type) {
-    return imports.getOrDefault(type, BUILTIN.getOrDefault(type, this.packageName));
+    return imports.getOrDefault(type, this.packageName);
   }
 
-  public Type of(com.github.javaparser.ast.type.Type type) {
-    String typeLiteral = type.toString();
-
-    if (isList(typeLiteral)) {
-      String tagType = extractTagType(typeLiteral);
-      String valType = extractType(typeLiteral);
-      return list(
-          new CanonicalName(resolvePackageName(tagType), tagType),
-          new CanonicalName(resolvePackageName(valType),  valType)
-      );
-    }
-
-    if (isMap(typeLiteral)) {
-      String tagType = extractTagType(typeLiteral);
-      String keyType = extractKeyType(typeLiteral);
-      String valType = extractValueType(typeLiteral);
-
-      return map(
-          new CanonicalName(resolvePackageName(tagType), tagType),
-          new CanonicalName(resolvePackageName(keyType), keyType),
-          new CanonicalName(resolvePackageName(valType), valType)
-      );
-    }
-
-    if (type instanceof PrimitiveType || typeLiteral.equals("char[]")) {
-      return object(new CanonicalName(typeLiteral));
-    } else {
-      return object(new CanonicalName(resolvePackageName(typeLiteral), typeLiteral));
-    }
-  }
-
-  private static boolean isList(String typeLiteral) {
-    return typeLiteral.startsWith("List<") && typeLiteral.endsWith(">");
-  }
-
-  private static boolean isMap(String typeLiteral) {
-    return typeLiteral.startsWith("Map<") && typeLiteral.endsWith(">");
-  }
-
-  private static String extractTagType(String typeLiteral) {
-    return typeLiteral.substring(0, typeLiteral.indexOf("<"));
-  }
-
-  private static String extractType(String typeLiteral) {
-    return typeLiteral.substring(5, typeLiteral.length() - 1);
-  }
-
-  private static String extractKeyType(String typeLiteral) {
-    int index = typeLiteral.indexOf(",");
-    return typeLiteral.substring(4, index);
-  }
-
-  private static String extractValueType(String typeLiteral) {
-    int index = typeLiteral.indexOf(",");
-    return typeLiteral.substring(index+2, typeLiteral.length() - 1);
-  }
 }
