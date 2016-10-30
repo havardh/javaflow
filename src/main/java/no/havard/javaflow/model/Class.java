cb46aaec791b5cc7a88342f4009612937a32a1bb
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2016-10-30 20:11:21 +0100

    refactor: simplified class construction and internal organization

--- src/main/java/no/havard/javaflow/model/Class.java
+++ src/main/java/no/havard/javaflow/model/Class.java
@@ -1,48 +1,35 @@
 package no.havard.javaflow.model;
 
 import static java.util.Collections.emptyList;
-import static java.util.stream.Collectors.toList;
 
 import static no.havard.javaflow.util.Lists.union;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Stream;
 
 public class Class extends Definition {
 
   private final Parent parent;
   private final List<Field> fields;
 
-  public Class(CanonicalName name, List<Field> definitions) {
+  public Class(CanonicalName name, Parent parent, List<Field> fields) {
     super(name);
-    this.parent = null;
-    this.fields = definitions;
-  }
-
-  public Class(
-      CanonicalName name,
-      CanonicalName parentName,
-      List<Field> definitions
-  ) {
-    super(name);
-    this.parent = new Parent(parentName);
-    this.fields = definitions;
+    this.parent = parent;
+    this.fields = fields;
   }
 
   public List<Field> getFields() {
-    return union(getParentFieldDefinitions(), fields);
+    return union(
+        getParentFields(),
+        fields
+    );
   }
 
-  private List<Field> getParentFieldDefinitions() {
-    return  getParent().map(p -> (p.getReference()).getFields()).orElse(emptyList());
+  private List<Field> getParentFields() {
+    return  getParent().map(Parent::getFields).orElse(emptyList());
   }
 
   public Optional<Parent> getParent() {
     return Optional.ofNullable(parent);
-  }
-
-  public void setParentRef(Class aClass) {
-    getParent().ifPresent(parent -> parent.setReference(aClass));
   }
 }
