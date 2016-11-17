1318b55e365533fc74bf0933c00f08dfe2c87578
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2016-11-17 20:50:59 +0100

    fix: add equals and hashCode to Type

--- src/main/java/no/havard/javaflow/ast/Type.java
+++ src/main/java/no/havard/javaflow/ast/Type.java
@@ -29,8 +29,27 @@ public class Type {
   }
 
   @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    Type type = (Type) o;
+
+    return name != null ? name.equals(type.name) : type.name == null;
+  }
+
+  @Override
+  public int hashCode() {
+    return name != null ? name.hashCode() : 0;
+  }
+
+  @Override
   public String toString() {
-    return format("Type[%s]", name);
+    return format("%s", name);
   }
 }
 
