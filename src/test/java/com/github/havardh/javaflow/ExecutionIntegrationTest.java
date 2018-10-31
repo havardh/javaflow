09b7eb7e50940c7d73aef65e78d616093de5dec4
Pascal <pascal.gruen@gmail.com>
2018-10-31 08:21:42 +0100

    fix: add additional test for comments on fields

--- src/test/java/com/github/havardh/javaflow/ExecutionIntegrationTest.java
+++ src/test/java/com/github/havardh/javaflow/ExecutionIntegrationTest.java
@@ -79,6 +79,7 @@ public class ExecutionIntegrationTest {
         "  field1: string,",
         "  field2: string,",
         "  field3: string,",
+        "  field4: string,",
         "};"
     );
   }
